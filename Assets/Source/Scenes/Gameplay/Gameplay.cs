fa3c81deb06e716ab72bb7255cea04aca319c1cf
Dawid Członka <dawid.czlonka@pixzell.pl>
2016-08-01 15:03:38 +0200

    Animate fox idle. Add base script for Gameplay scene.

---
+++ Assets/Source/Scenes/Gameplay/Gameplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Gameplay : MonoBehaviour
+{
+	void Start ()
+    {
+	    
+	}
+
+    /* ui event */ public void OnExitClick()
+    {
+        GoToMainMenu();
+    }
+
+    private void GoToMainMenu()
+    {
+        SceneManager.LoadScene(Constant.SCENE_MENU);
+    }
+}
