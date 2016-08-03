83ca4584a742d59d88fd545e076fab374ab1a618
Sebastian Sztangierski <sebastian.sztangierski@pixzell.pl>
2016-08-03 13:25:21 +0200

    [Problem / Solution] Add shuffle method for shuffling questions

---
+++ Assets/Source/Extensions/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ListExtensions {
+
+	public static void Shuffle<T>(this IList<T> list) {
+		int n = list.Count;
+		Random rnd = new Random();
+		while (n > 1) {
+			int k = (rnd.Next(0, n) % n);
+			n--;
+			T value = list[k];
+			list[k] = list[n];
+			list[n] = value;
+		}
+	}
+}
