0137026511f455345182153d9285c56c375cf80d
Sebastian Sztangierski <sebastian.sztangierski@pixzell.pl>
2016-08-05 11:40:51 +0200

    [Problem / Solution] Add Syncano client class, Syncano exception and Response classes for communication with Syncano

---
+++ Assets/Source/Syncano/Data/Response.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Response : JsonData<Response>
+{
+	/// <summary>
+	/// The response status. Returns success or fail string.
+	/// </summary>
+	public string status;
+
+	/// <summary>
+	/// The duration of processing request.
+	/// </summary>
+	public int duration;
+
+	/// <summary>
+	/// The serialized result.
+	/// </summary>
+	public Result result;
+
+	/// <summary>
+	/// Shortcut to get JSON response.
+	/// </summary>
+	public string resultJSON { get { return result.stdout;}}
+
+	/// <summary>
+	/// Class for holding error and output string.
+	/// </summary>
+	[System.Serializable]
+	public class Result
+	{
+		/// <summary>
+		/// Error message.
+		/// </summary>
+		public string stderr;
+
+		/// <summary>
+		/// Serialized Output in JSON.
+		/// </summary>
+		public string stdout;
+
+	}
+}
---
+++ Assets/Source/Syncano/SyncanoClient.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.Networking;
+
+public class SyncanoClient : SelfInstantiatingSingleton<SyncanoClient>
+{
+	/// <summary>
+	/// The name of the instance.
+	/// </summary>
+	public string InstanceName;
+
+	/// <summary>
+	/// The API key.
+	/// </summary>
+	public string ApiKey;
+
+	private string baseUrl;
+
+	public IEnumerator CallScriptEndpoint(string endpointId, string scriptName, System.Action<Response> callback)
+	{
+		baseUrl = string.Format("https://api.syncano.io/v1.1/instances/{0}/", InstanceName);
+
+		StringBuilder sb = new StringBuilder(baseUrl);
+		sb.Append("endpoints/scripts/p/");
+		sb.Append(endpointId);
+		sb.Append("/");
+		sb.Append(scriptName);
+		sb.Append("/");
+
+		UnityWebRequest webRequest = UnityWebRequest.Get(sb.ToString());
+
+		yield return webRequest.Send();
+
+		if(webRequest.isError)
+		{
+			throw new SyncanoException(webRequest.error);
+		}
+
+		else
+		{
+			Response response = Response.FromJson(webRequest.downloadHandler.text);
+			callback (response);
+		}
+	}
+}
