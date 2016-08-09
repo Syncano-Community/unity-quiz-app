cd4c8adf3fa8777b19683a47b1b82f2325ba1863
Dawid Członka <dawid.czlonka@pixzell.pl>
2016-08-09 13:12:06 +0200

    Audience lifeline view.

--- Assets/Source/Scenes/Gameplay/Lifelines/AudiencePanel.cs
+++ Assets/Source/Scenes/Gameplay/Lifelines/AudiencePanel.cs
@@ -4,5 +4,15 @@ using System.Collections;
 
 public class AudiencePanel : LifelinePanelBase
 {
-    
+    [SerializeField]
+    private ChartBarView[] chartBars = new ChartBarView[4];
+
+    public override void Show (Question question)
+    {
+        base.Show (question);
+        chartBars[0].SetPercentage(7);
+        chartBars[1].SetPercentage(32);
+        chartBars[2].SetPercentage(55);
+        chartBars[3].SetPercentage(6);
+    }
 }
---
+++ Assets/Source/Scenes/Gameplay/Views/ChartBarView.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChartBarView : MonoBehaviour
+{
+    [SerializeField]
+    private Text percentageText;
+
+    [SerializeField]
+    private Image fillImage;
+
+    /// <summary>
+    /// Sets the percentage (0 - 100).
+    /// </summary>
+    public void SetPercentage(int percent)
+    {
+        StartCoroutine(AnimatePercentage(percent));
+    }
+
+    public IEnumerator AnimatePercentage(int percent)
+    {
+        float value = 0;
+
+        while (true)
+        {
+            value += 40f * Time.deltaTime;
+            fillImage.fillAmount = (float)value / 100.0f;
+            percentageText.text = ((int)value).ToString() + "%";
+
+            if (value >= percent)
+                break;
+
+            yield return null;
+        }
+    }
+}
