23b99cdd450f9de239480d7091503e7f118cfb11
Dawid Członka <dawid.czlonka@pixzell.pl>
2016-08-18 12:27:55 +0200

    Add $ sign to score table.

--- Assets/Source/General/ScoreTable.cs
+++ Assets/Source/General/ScoreTable.cs
@@ -6,21 +6,21 @@ public class ScoreTable
     public static readonly ScoreRow zero = new ScoreRow("0", 0, true);
 
     public static readonly ScoreRow[] rows = {
-        /* 01 */ new ScoreRow("100", 100, false),
-        /* 02 */ new ScoreRow("200", 200, false),
-        /* 03 */ new ScoreRow("300", 300, false),
-        /* 04 */ new ScoreRow("500", 500, false),
-        /* 05 */ new ScoreRow("1 000", 1000, true),
-        /* 06 */ new ScoreRow("2 000", 2000, false),
-        /* 07 */ new ScoreRow("4 000", 4000, false),
-        /* 08 */ new ScoreRow("8 000", 8000, false),
-        /* 09 */ new ScoreRow("16 000", 16000, false),
-        /* 10 */ new ScoreRow("32 000", 32000, true),
-        /* 11 */ new ScoreRow("64 000", 64000, false),
-        /* 12 */ new ScoreRow("125 000", 125000, false),
-        /* 13 */ new ScoreRow("250 000", 250000, false),
-        /* 14 */ new ScoreRow("500 000", 500000, false),
-        /* 15 */ new ScoreRow("1 MILION", 1000000, true),
+        /* 01 */ new ScoreRow("$100", 100, false),
+        /* 02 */ new ScoreRow("$200", 200, false),
+        /* 03 */ new ScoreRow("$300", 300, false),
+        /* 04 */ new ScoreRow("$500", 500, false),
+        /* 05 */ new ScoreRow("$1,000", 1000, true),
+        /* 06 */ new ScoreRow("$2,000", 2000, false),
+        /* 07 */ new ScoreRow("$4,000", 4000, false),
+        /* 08 */ new ScoreRow("$8,000", 8000, false),
+        /* 09 */ new ScoreRow("$16,000", 16000, false),
+        /* 10 */ new ScoreRow("$32,000", 32000, true),
+        /* 11 */ new ScoreRow("$64,000", 64000, false),
+        /* 12 */ new ScoreRow("$125,000", 125000, false),
+        /* 13 */ new ScoreRow("$250,000", 250000, false),
+        /* 14 */ new ScoreRow("$500,000", 500000, false),
+        /* 15 */ new ScoreRow("$1 MILION", 1000000, true),
     };
 
     public static ScoreRow GetRow(int index)
