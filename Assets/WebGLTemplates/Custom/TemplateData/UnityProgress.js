fe88cd2dbeb4a18523994f686d4299ec47b16d21
Dawid Członka <dawid.czlonka@pixzell.pl>
2016-08-24 16:46:58 +0200

    Update progress bar.

--- Assets/WebGLTemplates/Custom/TemplateData/UnityProgress.js
+++ Assets/WebGLTemplates/Custom/TemplateData/UnityProgress.js
@@ -5,20 +5,13 @@ function UnityProgress (dom) {
     var parent = dom.parentNode;
     
     this.SetProgress = function (progress) {
-        if (this.progress < progress)
-            this.progress = progress;
-        if (progress == 1) {
-            this.SetMessage("Preparing...");
-            document.getElementById("bgBar").style.display = "none";
-            document.getElementById("progressBar").style.display = "none";
-        }
-        
+        this.progress = progress;
         this.Update();
     }
     
     this.SetMessage = function (message) {
         this.message = message;
-        this.Update();
+        document.getElementById("loadingInfo").innerHTML = this.message;
     }
     
     this.Clear = function() {
@@ -29,7 +22,6 @@ function UnityProgress (dom) {
         var length = 200 * Math.min(this.progress, 1);
         bar = document.getElementById("progressBar");
         bar.style.width = length + "px";
-        document.getElementById("loadingInfo").innerHTML = this.message;
     }
     
     this.Update ();
